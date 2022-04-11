4e2cef847e62aacdc8fc13ceac1525c7a666f439
Veera3112 <praveenkumarrslss@gmail.com>
2022-04-11 16:01:54 +0530

    first commit

---
+++ MMS-23/src/main/java/team/mms23/mms/Mms23Application.java
@@ -0,0 +1,13 @@
+package team.mms23.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mms23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mms23Application.class, args);
+	}
+
+}
---
+++ MMS-23/src/test/java/team/mms23/mms/Mms23ApplicationTests.java
@@ -0,0 +1,13 @@
+package team.mms23.mms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mms23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
